62a39e4fa309827569f91d2ee3236b58769451c8
C Baucom <chrisrb83@gmail.com>
2018-11-21 01:20:39 -0500

    Charging credit cards on the server side

--- src/stripe.js
+++ src/stripe.js
@@ -1,1 +1,6 @@
-module.exports = require('stripe')(process.env.STRIPE_SECRET);
+module.exports = require("stripe")(process.env.STRIPE_SECRET);
+
+// The above is a simplified version of the following
+// const stripe = require('stripe');
+// const config = stripe(process.env.STRIPE_SECRET);
+// module.exports = config;
