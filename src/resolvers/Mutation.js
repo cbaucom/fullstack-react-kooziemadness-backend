fce2d70d3fec614de54aa49ff67147ede6d35712
C Baucom <chrisrb83@gmail.com>
2018-11-26 16:03:27 -0500

    add update password

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -336,6 +336,44 @@ const Mutations = {
     });
     // 7. Return the Order to the client
     return order;
+  },
+  async updatePassword(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!");
+    }
+    // 2. Query the current user
+    const user = await ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId
+        }
+      },
+      info
+    );
+    // const user = await ctx.db.query.user({ where: { email } });
+    // 3. check if the passwords match
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Passwords don't match!");
+    }
+    // 4. Hash their new password
+    const password = await bcrypt.hash(args.password, 10);
+    // 5. Save the new password to the user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password
+      }
+    });
+    // 6. Generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    // 7. Set the JWT cookie
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // 8. return the new user
+    return updatedUser;
   }
 };
 
